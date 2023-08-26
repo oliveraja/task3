6c08032f0e89e7eb49daddea363eaf5371f80b39
Oliver <oliversebastian321@gmail.com>
2023-08-27 01:01:17 +0700

    update tugas

---
+++ tugas.js
@@ -0,0 +1,14 @@
+let nameUser = "Oliver Sebastian";
+console.log("nama : ", nameUser.length);
+
+let upperCaseName = nameUser.toUpperCase();
+console.log("uppercase : ", upperCaseName);
+
+let lowerCaseName = nameUser.toLowerCase();
+console.log("lowercase : ", lowerCaseName);
+
+let fistWord = nameUser;
+console.log("huruf pertama : ", fistWord[0]);
+
+let slicingName = nameUser.slice(0, 5);
+console.log("nama depan : ", slicingName);
